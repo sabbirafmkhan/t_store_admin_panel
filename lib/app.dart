7bdb4647f1706ba11a6b75999f8b4576a68d9c74
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-05-24 16:09:08 +0600

    setup TStoreAdminPanelApp

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TStoreAdminPanelApp extends StatelessWidget {
+  const TStoreAdminPanelApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:t_store_admin_panel/app.dart';
+
+/// Entry point of Flutter App
+Future<void> main() async {
+  // Ensure that widgets are initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize GetX Local Storage
+
+  // Remove # sign from url
+
+  // Initialize Firebase & Authentication Repository
+
+  // Main App Starts here...
+  runApp(const TStoreAdminPanelApp());
+}
