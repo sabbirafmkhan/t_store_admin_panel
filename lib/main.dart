f8590f225684467c0464c5f42f89e1d6b037f9cd
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-05-24 18:31:18 +0600

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store_admin_panel/app.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 /// Entry point of Flutter App
 Future<void> main() async {
@@ -11,6 +14,8 @@ Future<void> main() async {
   // Remove # sign from url
 
   // Initialize Firebase & Authentication Repository
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // .then((_) => Get.put(AuthenticationRepository()));
 
   // Main App Starts here...
   runApp(const TStoreAdminPanelApp());
