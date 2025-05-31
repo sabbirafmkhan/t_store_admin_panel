bea6ed3b0e0dca9272d0793e87a48b3c36804b62
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-05-31 20:36:14 +0600

    Dashboard card

--- lib/features/authentication/screens/dashboard/responsive_screen/dashboard_desktop.dart
+++ lib/features/authentication/screens/dashboard/responsive_screen/dashboard_desktop.dart
@@ -1,8 +1,5 @@
-import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:t_store_admin_panel/common/widgets/data_table/paginated_data_table.dart';
+import 'package:t_store_admin_panel/features/authentication/screens/dashboard/widgets/dashboard_card.dart';
 import 'package:t_store_admin_panel/utils/constants/sizes.dart';
 
 class DashboardDesktopScreen extends StatelessWidget {
@@ -10,152 +7,59 @@ class DashboardDesktopScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final DashboardController controller = Get.put(DashboardController());
-
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Center(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TextFormField(
-                controller: controller.searchTextController,
-                onChanged: (query) => controller.searchQuery(query),
-                decoration: const InputDecoration(
-                  hintText: 'Search',
-                  prefixIcon: Icon(Iconsax.search_normal),
-                ),
+              // Heading:
+              Text(
+                'Dashboard',
+                style: Theme.of(context).textTheme.headlineLarge,
               ),
               const SizedBox(height: TSizes.spaceBtwSections),
-              Obx(
-                () {
-                  // orders and selected rows are hidden => just to update the UI=> obx => [productRows]
-                  Visibility(
-                    visible: false,
-                    child: Text(controller.filteredDataList.length.toString()),
-                  );
 
-                  return Expanded(
-                    child: TPaginatedDataTable(
-                      sortAscending: controller.sortAscending.value,
-                      sortColumnIndex: controller.sortColumnIndex.value,
-                      columns: [
-                        const DataColumn2(label: Text('Column 1')),
-                        DataColumn(
-                            label: const Text('Column 2'),
-                            onSort: (columnIndex, ascending) =>
-                                controller.sortById(columnIndex, ascending)),
-                        DataColumn(
-                            label: const Text('Column 3'),
-                            onSort: (columnIndex, ascending) =>
-                                controller.sortById(columnIndex, ascending)),
-                        const DataColumn(label: Text('Column 4')),
-                      ],
-                      source: MyData(),
+              // Cards:
+              const Row(
+                children: [
+                  Expanded(
+                    child: TDashboardCard(
+                      title: 'Sales total',
+                      subTitle: '\$365.6',
+                      stats: 25,
                     ),
-                  );
-                },
+                  ),
+                  SizedBox(width: TSizes.spaceBtwItems),
+                  Expanded(
+                    child: TDashboardCard(
+                      title: 'Average Order Value',
+                      subTitle: '\$25',
+                      stats: 15,
+                    ),
+                  ),
+                  SizedBox(width: TSizes.spaceBtwItems),
+                  Expanded(
+                    child: TDashboardCard(
+                      title: 'Total Order',
+                      subTitle: '36',
+                      stats: 44,
+                    ),
+                  ),
+                  SizedBox(width: TSizes.spaceBtwItems),
+                  Expanded(
+                    child: TDashboardCard(
+                      title: 'Visitors',
+                      subTitle: '25035',
+                      stats: 2,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class MyData extends DataTableSource {
-  final DashboardController controller = Get.put(DashboardController());
-
-  @override
-  DataRow? getRow(int index) {
-    final data = controller.filteredDataList[index];
-    return DataRow2(
-      onTap: () {},
-      selected: controller.selectedRows[index],
-      onSelectChanged: (value) =>
-          controller.selectedRows[index] = value ?? false,
-      cells: [
-        DataCell(Text(data['Column1'] ?? '')),
-        DataCell(Text(data['Column2'] ?? '')),
-        DataCell(Text(data['Column3'] ?? '')),
-        DataCell(Text(data['Column4'] ?? '')),
-      ],
-    );
-  }
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get rowCount => controller.filteredDataList.length;
-
-  @override
-  int get selectedRowCount => 0;
-}
-
-/// -----------DashboardController
-class DashboardController extends GetxController {
-  var dataList = <Map<String, String>>[].obs;
-  var filteredDataList = <Map<String, String>>[].obs;
-  RxList<bool> selectedRows = <bool>[].obs;
-
-  RxInt sortColumnIndex = 1.obs;
-  RxBool sortAscending = true.obs;
-  final searchTextController = TextEditingController();
-
-  @override
-  void onInit() {
-    super.onInit();
-    fetchDummyData();
-  }
-
-  void searchQuery(String query) {
-    filteredDataList.assignAll(dataList
-        .where((item) => item['column1']!.contains(query.toLowerCase())));
-  }
-
-  void sortById(int sortColumnIndex, bool ascending) {
-    sortAscending.value = ascending;
-    filteredDataList.sort((a, b) {
-      if (ascending) {
-        return filteredDataList[0]['column1']
-            .toString()
-            .toLowerCase()
-            .compareTo(filteredDataList[0]['column1'].toString().toLowerCase());
-      } else {
-        return filteredDataList[0]['column1']
-            .toString()
-            .toLowerCase()
-            .compareTo(filteredDataList[0]['column1'].toString().toLowerCase());
-      }
-    });
-    this.sortColumnIndex.value = sortColumnIndex;
-  }
-
-  void fetchDummyData() {
-    selectedRows.assignAll(List.generate(36, (index) => false));
-    dataList.addAll(
-      List.generate(
-        36,
-        (index) => {
-          'Column1:': 'Data ${index + 1} -1',
-          'Column2:': 'Data ${index + 1} -2',
-          'Column3:': 'Data ${index + 1} -3',
-          'Column4:': 'Data ${index + 1} -4',
-        },
-      ),
-    );
-
-    filteredDataList.addAll(
-      List.generate(
-        36,
-        (index) => {
-          'Column1:': 'Data ${index + 1} -1',
-          'Column2:': 'Data ${index + 1} -2',
-          'Column3:': 'Data ${index + 1} -3',
-          'Column4:': 'Data ${index + 1} -4',
-        },
       ),
     );
   }
--- lib/features/authentication/screens/dashboard/responsive_screen/dashboard_mobile.dart
+++ lib/features/authentication/screens/dashboard/responsive_screen/dashboard_mobile.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:t_store_admin_panel/features/authentication/screens/dashboard/widgets/dashboard_card.dart';
+import 'package:t_store_admin_panel/utils/constants/sizes.dart';
 
 class DashboardMobileScreen extends StatelessWidget {
   const DashboardMobileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Mobile"),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Heading:
+              Text(
+                'Dashboard',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Cards:
+              const TDashboardCard(
+                title: 'Sales total',
+                subTitle: '\$365.6',
+                stats: 25,
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              const TDashboardCard(
+                title: 'Average Order Value',
+                subTitle: '\$25',
+                stats: 15,
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              const TDashboardCard(
+                title: 'Total Order',
+                subTitle: '36',
+                stats: 44,
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              const TDashboardCard(
+                title: 'Visitors',
+                subTitle: '25035',
+                stats: 2,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
